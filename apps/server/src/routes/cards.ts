e3ac33332cf57783dfe1bc1501090e7b3873a13b
Dongho Gang <gongdongho12@gmail.com>
2023-10-21 22:35:48 +0900

    feat: url 에 따른 내용 업데이트 기능 추가

--- apps/server/src/routes/cards.ts
+++ apps/server/src/routes/cards.ts
@@ -1,15 +1,52 @@
-import express, { Request, Response, Router } from 'express';
-import svg_generator from '../cards/svg_generator';
+import express, { Request, Response, Router } from "express";
+import createMetascraper from "metascraper";
+import metascraperDescription from "metascraper-description";
+import metascraperImage from "metascraper-image";
+import metascraperTitle from "metascraper-title";
+import axios from "axios";
+import svg_generator from "../cards/svg_generator";
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-    return res.status(200).send(svg_generator({
-      title: '제목',
-      description: '내용',
-      tags: ['테스트1', '테스트2'],
-      username: 'teddy'
-    }));
+const tagsRule: any = {
+	tags: [
+		({ htmlDom: $, url }: any) =>
+			$(".categories > .category")
+				.map(function () {
+          // @ts-ignore
+					return $(this).text();
+				})
+				.toArray(),
+	],
+};
+
+const metascraper = createMetascraper([
+	metascraperDescription(),
+	metascraperImage(),
+	metascraperTitle(),
+  (() => tagsRule) as any
+]);
+
+router.get("/", async (req: Request, res: Response) => {
+	const url =
+		"https://blog.dongholab.com/2023-swuniv-hackathon-mentor-retrospection/";
+	const { title, description, image } = await axios
+		.get(url, { params: {} })
+		.then((res) => {
+			const html = res.data;
+			console.log("html", html);
+			return { html, url };
+		})
+		.then(metascraper)
+
+	return res.status(200).send(
+		svg_generator({
+			title,
+			description,
+			tags: ["테스트1", "테스트2"],
+			username: "teddy",
+		})
+	);
 });
 
 export default router;
